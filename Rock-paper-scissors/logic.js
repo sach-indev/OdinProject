da8ee9f010868c20137fb64519580b8d9a71dcfb
Sachin Choudhary <sachinchoudharyboss24@gmail.com>
2026-06-21 13:13:15 +0530

    added ui to rock paper scissors

--- Rock-paper-scissors/logic.js
+++ Rock-paper-scissors/logic.js
@@ -1,73 +1,156 @@
-"use strict";
-
-//ok so how do we go about this. First user input that would be most definetely prompt with parseInt to get 1, 2, and 3 choices for rock, paper and scisors. Now we can also make the user choose how many games to play. Ok And lastly use math.random to get computer choice. Now after that compare with user choice and print the result. Keep score and lastly print the final score.
+"use strict"
 
 
-alert("This is a Tournament of Rock, Paper and Sissors!!!")
-let numGames = parseInt(prompt("Please enter the number of games to be played in Tournamnet: "));
-
-let playerScore = 0, compScore = 0;
-for(let i = 0; i<numGames; i++)
+let playRound = (playerChoice) =>
 {
-    if(i==0) alert("Starting " +  (i+1) + "st Game..." );
-    else if(i==1) alert("Starting " +  (i+1) + "nd Game..." );
-    else alert("Starting " +  (i+1) + "rd Game..." );
+    let compChoice = Math.floor(Math.random()*3);
 
-    let playerChoice = parseInt(prompt("Enter Your choice({1: Rock, 2: Paper, 3: Scissors): "));
+    if(playerChoice==compChoice) return 0;
 
-    let compChoice = Math.floor(Math.random()*3) + 1;
-
-    if(playerChoice===compChoice)
+    else if(Math.abs(playerChoice - compChoice)==1)
     {
-        alert("Its a tie!!!");
-        playerScore++;
-        compScore++;
+        if(playerChoice > compChoice) return 1;
+        else return 2;
     }
-
     else
     {
-        if(Math.abs(playerChoice - compChoice) == 1)
+        if(playerChoice > compChoice) return 2;
+        else return 1;
+    }
+};
+
+const startBtn = document.querySelector('.start-btn');
+const games = document.querySelector('.games');
+
+let playerScore = 0, compScore = 0, round = 1;
+
+function clearGames()
+{
+    games.innerHTML = "";
+}
+
+function createRound()
+{
+    clearGames();
+
+    const roundTitle = document.createElement('h1');
+    roundTitle.className = 'round-title';
+    roundTitle.innerText = `Round ${round} : Make Your Choice`;
+
+    const choices = document.createElement('div');
+    choices.className = 'choices';
+
+    const rock = document.createElement('button');
+    const paper = document.createElement('button');
+    const scissors = document.createElement('button');
+
+    rock.innerText = 'Rock';
+    paper.innerText = 'Paper';
+    scissors.innerText = 'Scissors';
+
+    choices.append(rock, paper, scissors);
+
+    const resultContainer = document.createElement('div');
+    resultContainer.className = 'round-result-container';
+
+    const resultBox = document.createElement('div');
+    resultBox.className = 'round-result';
+
+    resultContainer.appendChild(resultBox); 
+
+    //buttons
+    const continueBtn = document.createElement('button');
+    continueBtn.className = 'continue';
+    continueBtn.innerText = "continue";
+    continueBtn.disabled = true;
+    
+    const finishBtn = document.createElement('button');
+    finishBtn.className = 'finish';
+    finishBtn.innerText = 'Finish';
+
+    //append Everything
+    games.append(roundTitle, choices, resultContainer, continueBtn, finishBtn);
+
+    function disableChoices()
+    {
+        [rock, paper, scissors].forEach(b => b.disabled = true);
+    }
+
+    function handleChoice(choice)
+    {
+        const win = playRound(choice);
+
+        disableChoices();
+        continueBtn.disabled = false;
+
+        if(win==0)
         {
-            if(playerChoice > compChoice)
-            {
-                alert("You Win!");
-                playerScore++;
-            }
-            else
-            {
-                alert("You Loose!");
-                compScore++;
-            }
+            resultBox.innerText = "IT'S A TIE";
+            playerScore++;
+            compScore++;
+        }
+
+        else if(win==1)
+        {
+            resultBox.innerText = "YOU WIN";
+            playerScore++;
         }
         else
         {
-            if(playerChoice > compChoice)
-            {
-                alert("You Loose!");
-                compScore++;
-            }
-            else
-            {
-                alert("You Win!");
-                playerScore++;
-            }
+            resultBox.innerText = "YOU LOOSE";
+            compScore++;
         }
     }
+
+    rock.onclick = ()=>handleChoice(0);
+    paper.onclick = ()=>handleChoice(1);
+    scissors.onclick = ()=>handleChoice(2);
+
+    continueBtn.onclick = ()=>
+    {
+        round++;
+        createRound();
+    };
+
+    finishBtn.onclick = ()=>
+    {
+        clearGames();
+        
+        const scoreContainer = document.createElement('div');
+        scoreContainer.className = 'score-container';
+        
+        const comp = document.createElement('div');
+        comp.className = 'comp-score';
+        comp.innerText = `Computer Score : ${compScore}`;
+
+        const player = document.createElement('div');
+        player.className = 'player-score';
+        player.innerText = `Player Score : ${playerScore}`;
+
+        scoreContainer.append(comp, player);
+
+        const finalContainer = document.createElement('div');
+        finalContainer.className = 'final-result-container';
+
+        const final = document.createElement('div');
+        final.className = 'final-result';
+
+        if(playerScore === compScore) final.innerText = 'TOURNAMENT TIE';
+        else if(playerScore > compScore) final.innerText = 'YOU WIN THE TOURNAMENT';
+        else final.innerText = 'YOU LOSE THE TOURNAMENT';
+
+        finalContainer.appendChild(final);
+
+        games.append(scoreContainer, finalContainer);
+
+    };
 }
 
-if(playerScore > compScore)
+startBtn.addEventListener('click', ()=>
 {
-    alert("You have won the tournament\n Your score: " + playerScore + "\n Computer Score: " + compScore);
-}
+    playerScore = 0;
+    compScore = 0;
+    round = 1;
 
-else if(playerScore < compScore)
-{
-    alert("Alas you lost! Better luck next time.\n Your score: " + playerScore + "\n Computer Score: " + compScore);
-}
-
-else
-{
-    alert("Once in a blue moon we got a tie. Maybe its good luck?\n Your score: " + playerScore + "\n Computer Score: " + compScore);
-}
-
-alert("Please reload the page to play again!!!");
+    createRound();
+});
